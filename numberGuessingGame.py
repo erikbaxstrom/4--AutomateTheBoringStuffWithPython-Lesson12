685bc765a0e259c933261480f15f35b4b22af334
erikbaxstrom <9889725+erikbaxstrom@users.noreply.github.com>
2022-01-27 23:36:04 -0800

    Update numberGuessingGame.py

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,5 +1,4 @@
 import random
-import sys
 
 # ask for users name
 print('What is your name?')
@@ -10,9 +9,10 @@ name = input()
 number = random.randint(1,20)
 print('Hello,', name, 'I am thinking of a number between 1 and 20')
 
+guess = '' # start with empty string for edge case where user inputs only garbage
 
 # ask for guesses
-for i in range(0,6): 
+for guessCount in range(1,7): # I forgot that it tells you i at the end, so this should be (1,7) instead of (0,6)
     print('Take a guess')
     try:
         guess = int(input())
@@ -27,8 +27,9 @@ for i in range(0,6):
         print('too low')
         continue # go back to start of loop to ask for another guess
     else: # if just right, finish
-        print('Good job! It took you', i, 'guesses.')
-        sys.exit()
-
-# if too many guesses, you failed
-print('Nope. The number was', number)
+#        sys.exit() # unnecessary. Just break out of the for loop and then check if number == guess
+        break
+if guess == number: # if they're the same, you guessed correctly
+    print('Good job', name + '! It took you', guessCount, 'guesses.') # this is how Al did it in the video. I'm very surprised that the counter guessCount is available after the for loop terminates. Also, print('string', int + 'string) works, but seems sketchy
+else: # if too many guesses, you failed
+    print('Nope. The number was', number)
